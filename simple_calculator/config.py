a8f0591bbc8fa462b495c0ee102c54d48087ed88
DuskSwan <aquark@foxmail.com>
2023-11-06 20:45:30 +0800

    add config.py

---
+++ simple_calculator/config.py
@@ -0,0 +1,10 @@
+class cfg:
+    def __init__(self):
+        self.name = "simple_calculator"
+        self.version = "1.0.0"
+        self.author = "Zhang San"
+    def show_info(self):
+        print("This is class cfg")
+        print("The calculator' name is " + self.name\
+            + ", version is " + self.version\
+            + ", author is " + self.author)
--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,8 +1,11 @@
 from package_calc import calc
 from package_show.module_showInfo import show_info
 from package_show.module_showRules import show_rules
+import config
 
 def multi_lines_input():
+    cfg = config.cfg()
+    cfg.show_info()
     show_info()
     show_rules()
     while True:
